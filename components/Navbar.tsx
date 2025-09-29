c5f81aacb3015638cf6474d111d5da6d5305aff0
Bayu Aji <itsbayua@gmail.com>
2025-09-29 11:16:25 +0700

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { Home, Sprout } from "lucide-react";
+import ModeToggle from "./ModeToggle";
+
+export default function Navbar() {
+    return (
+        <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="flex justify-between items-center h-16">
+                    {/* Logo */}
+                    <div className="flex items-center">
+                        <Link
+                            href={"/"}
+                            className="text-xl font-bold text-primary font-mono tracking-wider"
+                        >
+                            🌱 Plantventory
+                        </Link>
+                    </div>
+                    {/* Navbar component */}
+                    <div className="hidden md:flex items-center space-x-4">
+                        <Button
+                            variant={"ghost"}
+                            className="flex items-center gap-2"
+                            asChild
+                        >
+                            <Link href={"/plants"}>
+                                <Sprout className="size-4" />
+                                <span className="hidden lg:inline">Plants</span>
+                            </Link>
+                        </Button>
+
+                        <Button
+                            variant={"ghost"}
+                            className="flex items-center gap-2"
+                            asChild
+                        >
+                            <Link href={"/"}>
+                                <Home className="size-4" />
+                                <span className="hidden lg:inline">Home</span>
+                            </Link>
+                        </Button>
+                        <ModeToggle />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
